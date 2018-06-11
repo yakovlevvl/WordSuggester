f1bb27bb2e6e69bbfd20db8deddf431487596653
Vladyslav Yakovlev <Vlad@macbook-air.local>
2018-06-11 15:05:53 +0300

    Initial Commit

---
+++ WordSuggester/WordSuggester.h
@@ -0,0 +1,19 @@
+//
+//  WordSuggester.h
+//  WordSuggester
+//
+//  Created by Vladyslav Yakovlev on 11.06.2018.
+//  Copyright © 2018 Vladyslav Yakovlev. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WordSuggester.
+FOUNDATION_EXPORT double WordSuggesterVersionNumber;
+
+//! Project version string for WordSuggester.
+FOUNDATION_EXPORT const unsigned char WordSuggesterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordSuggester/PublicHeader.h>
+
+
